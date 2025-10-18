47499e427a740c9a991d195a00d574c9637dcf93
Andy Hardy <aohardy@gmail.com>
2025-10-18 13:25:52 -0400

    add vercel setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercelServerless from '@astrojs/vercel/serverless';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercelServerless({
+    maxDuration: 8
+  }),
   vite: {
     plugins: [tailwindcss()]
   },
-  site: 'https://www.ultimatequestioncoding.com',
 });
